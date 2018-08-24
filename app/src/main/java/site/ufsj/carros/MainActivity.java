be0abeb41e8efcab73a6c70c6fa58334b26945e7
tuliomoreira77 <39540679+tuliomoreira77@users.noreply.github.com>
2018-08-24 18:32:55 -0300

    Update MainActivity.java

--- app/src/main/java/site/ufsj/carros/MainActivity.java
+++ app/src/main/java/site/ufsj/carros/MainActivity.java
@@ -1,46 +1,83 @@
 package site.ufsj.carros;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.io.InputStream;
 import java.util.HashMap;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
 
 public class MainActivity extends Activity {
 
+    Vector<Boolean> presenca = new Vector<Boolean>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<HashMap<String, String>> carros = new ArrayList<HashMap<String, String>>();
+        InputStream inputStream = getResources().openRawResource(R.raw.alunos);
+        CSVFile csvFile = new CSVFile(inputStream);
+        List<String[]> alunosList = csvFile.read();
 
-        for(int i = 0; i < 10; i++){
+        ArrayList<HashMap<String, String>> alunos = new ArrayList<HashMap<String, String>>();
 
-            HashMap<String, String> carro = new HashMap<String, String>();
-            carro.put("carro", "Carro "+(i + 1));
-            carro.put("marca", "Marca "+(i + 1));
+        for(int i = 0; i < alunosList.size(); i++){
 
-            carros.add(carro);
+            HashMap<String, String> aluno = new HashMap<String, String>();
+            aluno.put("Aluno", alunosList.get(i)[1]);
+            aluno.put("Matricula", alunosList.get(i)[0]);
+            presenca.add(true);
+            alunos.add(aluno);
         }
 
-        String[] from = new String[]{"carro", "marca"};
+        String[] from = new String[]{"Aluno", "Matricula"};
 
         int layout = R.layout.carros;
 
         int[] to = new int[]{R.id.t1, R.id.t2};
 
         ListView lv = (ListView) findViewById(R.id.lv);
-        lv.setAdapter(new SimpleAdapter(this, carros, layout, from, to));
+        lv.setAdapter(new SimpleAdapter(this, alunos, layout, from, to));
+        lv.setOnItemClickListener(mudaCor());
     }
+
+    public AdapterView.OnItemClickListener mudaCor(){
+        return(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int position, long id) {
+                Intent intent;
+
+                switch(position){
+                    default:
+                        if(presenca.get(position)) {
+                            v.setBackgroundColor(Color.GREEN);
+                            presenca.set(position, false);
+                        }
+                        else {
+                            v.setBackgroundColor(Color.RED);
+                            presenca.set(position, true);
+                        }
+                        break;
+                }
+            }
+
+        });
+    }
+
 
     public void sair(View view){
         finish();
     }
+}
 }
